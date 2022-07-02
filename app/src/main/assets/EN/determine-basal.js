46b521ef12cdd2809d40eaa671dc5f72f054bcf7
DICKO <mark@servecold.net>
2022-07-02 10:21:51 +1000

    ENWIOBThreshOK - Allows EN window to be activated when IOB > preference setting or there has been at least one recent manual bolus or SMB.

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -372,11 +372,14 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     var cTime = (( new Date(systemTime).getTime() - meal_data.lastCarbTime) / 60000); // last carb entry after EN start
     var b1Time = (typeof meal_data.firstENBolusTime !== 'undefined' ? (( new Date(systemTime).getTime() - meal_data.firstENBolusTime) / 60000) : 9999); // first normal bolus after EN start
     var bTime = (typeof meal_data.lastENBolusTime !== 'undefined' ? (( new Date(systemTime).getTime() - meal_data.lastENBolusTime) / 60000) : 9999); // last normal bolus after EN start
-    // ENWIOBThreshOK if there is enough IOB to trigger the EN window
-    var ENWIOBThreshU = profile.current_basal * profile.ENWIOBTrigger/60, ENWIOBThreshOK = (ENactive && ENWIOBThreshU > 0 && iob_data.iob > ENWIOBThreshU);
+    // minutes since last bolus - relocated
+    var lastBolusAge = round(( new Date(systemTime).getTime() - iob_data.lastBolusTime ) / 60000,1);
+    // ENWTriggerOK if there is enough IOB to trigger the EN window or we had a recent SMB
+    //var ENWIOBThreshU = profile.current_basal * profile.ENWIOBTrigger/60, ENWTriggerOK = (ENactive && ENWIOBThreshU > 0 && iob_data.iob > ENWIOBThreshU);
+    var ENWIOBThreshU = profile.current_basal * profile.ENWIOBTrigger/60, ENWTriggerOK = (ENactive && ENWIOBThreshU > 0 && (iob_data.iob > ENWIOBThreshU || lastBolusAge < 10));
     // ENWindowOK is when there is a recent COB entry or manual bolus
-    var ENWindowOK = (ENactive && profile.ENWindow > 0 && Math.min(c1Time, cTime ,bTime, b1Time) < profile.ENWindow || (profile.temptargetSet && target_bg <= normalTarget) || ENWIOBThreshOK);
-    if (!COB && (Math.min(b1Time,bTime) > profile.ENWindow) && !profile.temptargetSet && !ENWIOBThreshOK) ENWindowOK = false; // if theres no COB and no recent bolus or TT then close the EN window
+    var ENWindowOK = (ENactive && profile.ENWindow > 0 && Math.min(c1Time, cTime ,bTime, b1Time) < profile.ENWindow || (profile.temptargetSet && target_bg <= normalTarget) || ENWTriggerOK);
+    if (!COB && (Math.min(b1Time,bTime) > profile.ENWindow) && !profile.temptargetSet && !ENWTriggerOK) ENWindowOK = false; // if theres no COB and no recent bolus or TT then close the EN window
 
     var ENWindowRunTime = Math.min(c1Time, cTime, bTime, b1Time);
 
@@ -1227,7 +1230,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     rT.reason += (ENWindowOK ? "On" : "Off");
     rT.reason += (firstMealWindow ? " Bkfst" : "");
     rT.reason += (ENWindowOK && ENWindowRunTime <= profile.ENWindow ? " " + round(ENWindowRunTime)+"/"+profile.ENWindow+"m" : "");
-    rT.reason += (ENWIOBThreshOK ? " IOB>" + round(ENWIOBThreshU,2) : "");
+    rT.reason += (!ENWTriggerOK && ENWIOBThreshU > 0 ? " IOB<" + round(ENWIOBThreshU,2) : "");
 
     // other EN stuff
     rT.reason += ", SR: " + (typeof autosens_data !== 'undefined' && autosens_data ? round(autosens_data.ratio,2) + "=": "") + sensitivityRatio;
@@ -1490,7 +1493,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         rT.insulinReq = insulinReq;
         //console.error(iob_data.lastBolusTime);
         // minutes since last bolus
-        var lastBolusAge = round(( new Date(systemTime).getTime() - iob_data.lastBolusTime ) / 60000,1);
+        //var lastBolusAge = round(( new Date(systemTime).getTime() - iob_data.lastBolusTime ) / 60000,1);
         //console.error(lastBolusAge);
         //console.error(profile.temptargetSet, target_bg, rT.COB);
         // only allow microboluses with COB or low temp targets, or within DIA hours of a bolus
@@ -1564,8 +1567,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                     if (DeltaPct > DeltaPctThreshold && !ENWindowOK) ENReason += ", DeltaPct > " + round(DeltaPctThreshold*100) + "% EN" + (ENWindowOK ? "W" : "") + "-SMB";
                 } else {
                     // prevent SMB when below target for UAM rises Hypo Rebound Protection :)
-                    insulinReqPct = (bg < threshold && ENWIOBThreshU == 0 ? 0 : insulinReqPct);
-                    ENReason += (insulinReqPct == 0 ? ", HypoSafety Restrict SMB" : "");
+                    insulinReqPct = (bg < threshold && ENWIOBThreshU == 0 ? 0 : insulinReqPct); // disable SMB when below target and no intent to use auto EN window trigger
+                    ENReason += (insulinReqPct == 0 ? ", HypoSafety No SMB" : "");
                     ENReason += (insulinReqPct != 0  && iob_data.iob < maxBolus * 0.75 ? ", Low IOB Restrict SMB" : "");
                     ENMaxSMB = Math.min(maxBolus,ENMaxSMB); // use the most restrictive
                 }
