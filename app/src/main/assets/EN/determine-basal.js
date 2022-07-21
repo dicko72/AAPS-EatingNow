0976464718793ea153e07a729c68d85a4abcba03
DICKO <mark@servecold.net>
2022-07-21 21:16:42 +1000

    show start and end time in date format

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -250,8 +250,9 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
 
     enlog += "nowhrs: " + nowhrs + ", now: " + now +"\n";
-    enlog += "ENStartOffset: " + ENStartOffset + ", ENStartTime: " + ENStartTime +"\n";
-    enlog += "ENEndOffset: " + ENEndOffset + ", ENEndTime: " + ENEndTime +"\n";
+    enlog += "ENStartOffset: " + ENStartOffset + ", ENEndOffset: " + ENEndOffset +"\n";
+    enlog += "ENStartTime: " + new Date(ENStartTime).toLocaleString() +"\n";
+    enlog += "ENEndTime: " + new Date(ENEndTime).toLocaleString() +"\n";
     enlog += "lastNormalCarbTime: " + meal_data.lastNormalCarbTime + ", lastENBolusTime: " + meal_data.lastENBolusTime +"\n";
     enlog += "lastNormalCarbAge: " + lastNormalCarbAge +"\n";
 
