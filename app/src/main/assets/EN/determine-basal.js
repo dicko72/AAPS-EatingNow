e85f1f7e1ecb2b3f3ba624a804793f1dbd932f78
DICKO <mark@servecold.net>
2022-07-31 08:06:23 +1000

    Reduced TBR and some rounding

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1690,7 +1690,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         }
 
         insulinScheduled = currenttemp.duration * (currenttemp.rate - basal) / 60;
-        if (insulinScheduled >= rate) { // if current temp would deliver >2x more than the required insulin, lower the rate
+        if (insulinScheduled >= rate - basal) { // if current temp would deliver than the required remaining insulin, lower the rate
             rT.reason += currenttemp.duration + "m@" + (currenttemp.rate).toFixed(2) + " &gt;"+rate+". Setting temp basal of " + rate + "U/hr. ";
             return tempBasalFunctions.setTempBasal(rate, 30, profile, rT, currenttemp);
         }
