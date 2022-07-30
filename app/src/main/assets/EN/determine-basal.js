48eb4bed39cfbc586474dadef90795875c59848d
DICKO <mark@servecold.net>
2022-07-30 17:19:25 +1000

    rate_reduced

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1678,11 +1678,9 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         var maxSafeBasal = tempBasalFunctions.getMaxSafeBasal(profile);
 
         // if more insulin has been calculated and an SMB given reduce the temp rate
-        var rate_reduced = rate;
         if (insulinReq > insulinReqOrig && rT.units > 0) {
             rate = basal + insulinReq - rT.units;
             rate = round_basal(rate, profile);
-            rate_reduced = rate;
             rT.reason += "rate reduced: "+round(rate, 2)+", ";
         }
 
@@ -1692,7 +1690,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         }
 
         insulinScheduled = currenttemp.duration * (currenttemp.rate - basal) / 60;
-        if (insulinScheduled >= rate_reduced) { // if current temp would deliver >2x more than the required insulin, lower the rate
+        if (insulinScheduled >= rate) { // if current temp would deliver >2x more than the required insulin, lower the rate
             rT.reason += currenttemp.duration + "m@" + (currenttemp.rate).toFixed(2) + " &gt;"+rate_reduced+". Setting temp basal of " + rate + "U/hr. ";
             return tempBasalFunctions.setTempBasal(rate, 30, profile, rT, currenttemp);
         }
