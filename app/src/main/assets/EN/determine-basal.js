364b2466b43a65ddbb0311d557c93c52d6b7a187
DICKO <mark@servecold.net>
2022-08-07 11:49:35 +1000

    smaller SMB for smaller delta within ENW

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1566,7 +1566,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
                 // ============== DELTA & IOB BASED RESTRICTIONS ==============
                 // if the delta is less than 4 and insulinReq_sens is stronger restrict larger SMB
-                if (insulinReq_sens < sens_normalTarget && Delta <= 4) ENMaxSMB = Math.min(maxBolus,ENMaxSMB); // use the most restrictive
+                if (insulinReq_sens < sens_normalTarget && delta <= 4) ENMaxSMB = Math.min(maxBolus,ENMaxSMB); // use the most restrictive
                 // ===================================================
 
                 if (ENtimeOK) {
