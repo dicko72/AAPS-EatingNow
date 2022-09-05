995fe6d9d7e4c82047afca0780f55cfbf2cee262
DICKO <mark@servecold.net>
2022-09-05 16:15:02 +1000

    comment out IOBpredBG, ZTpredBG and UAMpredBG use dynamicISF

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -951,11 +951,11 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             // for IOBpredBGs, predicted deviation impact drops linearly from current deviation down to zero
             // over 60 minutes (data points every 5m)
             var predDev = ci * ( 1 - Math.min(1,IOBpredBGs.length/(60/5)) );
-            //IOBpredBG = IOBpredBGs[IOBpredBGs.length-1] + predBGI + predDev;
-            IOBpredBG = IOBpredBGs[IOBpredBGs.length-1] + (round(( -iobTick.activity * sens_normalTarget / (Math.log(IOBpredBGs[IOBpredBGs.length-1]/ins_val)+1) * 5 ),2)) + predDev;
+            IOBpredBG = IOBpredBGs[IOBpredBGs.length-1] + predBGI + predDev;
+            //IOBpredBG = IOBpredBGs[IOBpredBGs.length-1] + (round(( -iobTick.activity * sens_normalTarget / (Math.log(IOBpredBGs[IOBpredBGs.length-1]/ins_val)+1) * 5 ),2)) + predDev;
             // calculate predBGs with long zero temp without deviations
-            //var ZTpredBG = ZTpredBGs[ZTpredBGs.length-1] + predZTBGI;
-            var ZTpredBG = ZTpredBGs[ZTpredBGs.length-1] + (round(( -iobTick.iobWithZeroTemp.activity * sens_normalTarget / (Math.log(ZTpredBGs[ZTpredBGs.length-1] /ins_val)+1) * 5 ), 2));
+            var ZTpredBG = ZTpredBGs[ZTpredBGs.length-1] + predZTBGI;
+            //var ZTpredBG = ZTpredBGs[ZTpredBGs.length-1] + (round(( -iobTick.iobWithZeroTemp.activity * sens_normalTarget / (Math.log(ZTpredBGs[ZTpredBGs.length-1] /ins_val)+1) * 5 ), 2));
             // for COBpredBGs, predicted carb impact drops linearly from current carb impact down to zero
             // eventually accounting for all carbs (if they can be absorbed over DIA)
             var predCI = Math.max(0, Math.max(0,ci) * ( 1 - COBpredBGs.length/Math.max(cid*2,1) ) );
@@ -984,8 +984,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                 //console.error(UAMpredBGs.length,slopeFromDeviations, predUCI);
                 UAMduration=round((UAMpredBGs.length+1)*5/60,1);
             }
-            //UAMpredBG = UAMpredBGs[UAMpredBGs.length-1] + predBGI + Math.min(0, predDev) + predUCI;
-            UAMpredBG = UAMpredBGs[UAMpredBGs.length-1] + (round(( -iobTick.activity * sens_normalTarget / (Math.log(UAMpredBGs[UAMpredBGs.length-1] /ins_val)+1) * 5 ),2)) + Math.min(0, predDev) + predUCI;
+            UAMpredBG = UAMpredBGs[UAMpredBGs.length-1] + predBGI + Math.min(0, predDev) + predUCI;
+            //UAMpredBG = UAMpredBGs[UAMpredBGs.length-1] + (round(( -iobTick.activity * sens_normalTarget / (Math.log(UAMpredBGs[UAMpredBGs.length-1] /ins_val)+1) * 5 ),2)) + Math.min(0, predDev) + predUCI;
             //console.error(predBGI, predCI, predUCI);
             // truncate all BG predictions at 4 hours
             if ( IOBpredBGs.length < 48) { IOBpredBGs.push(IOBpredBG); }
