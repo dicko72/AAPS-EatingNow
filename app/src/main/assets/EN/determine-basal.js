722b26dcafcec7c8f0434c720a0ea4509fc3e382
DICKO <mark@servecold.net>
2022-09-19 07:03:25 +1000

    debug

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1158,7 +1158,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     // EN TT active and no bolus yet with UAM increase insulinReq_bg to provide initial insulinReq to 50% peak minutes of delta, max 90
     var insulinReq_boost = (ENTTActive && lastBolusAge >= ttTime && !COB);
-    //var endebug = "DEBUG: "+ttTime+", "+lastBolusAge+", "+minAgo+";";
+    var endebug = "DEBUG: "+ttTime+", "+lastBolusAge+", "+minAgo+";";
     var insulinReq_bg_boost = (insulinReq_boost && delta >= 0 ? profile.UAMbgBoost : 0);
 
     // categorize the eventualBG prediction type for more accurate weighting
