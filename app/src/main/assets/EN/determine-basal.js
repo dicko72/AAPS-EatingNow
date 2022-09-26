80fb816ed9a6b547a010f0ecfee2592b17ef33c0
DICKO <mark@servecold.net>
2022-09-26 17:02:21 +1000

    insulinReqTBR only when eventualBG is higher than insulinReq_bg

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1497,7 +1497,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         insulinReq = (insulinReq_bg - target_bg) / insulinReq_sens;
 
         // EXPERIMENT DEBUG ONLY - insulinReqTBR is the delta of full insulinReq up to eventualBG
-        var insulinReqTBR = (ENactive ? ((eventualBG - target_bg) / insulinReq_sens) - insulinReq : 0);
+        var insulinReqTBR = (ENactive && eventualBG > insulinReq_bg ? ((eventualBG - target_bg) / insulinReq_sens) - insulinReq : 0);
         var endebug = "DEBUG: "+insulinReqTBR+";";
         // insulinReqTBR = 0;
 
