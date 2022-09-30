5c4ab84b831f85558764da52a12e9b100839ccf0
DICKO <mark@servecold.net>
2022-09-30 10:28:57 +1000

    missing ;

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -1602,7 +1602,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                         //ENReason += ", Recent COB " + (profile.temptargetSet && target_bg == normalTarget ? " + TT" : "") + " ENW-SMB";
                     } else {
                         ENMaxSMB = (firstMealWindow ? profile.EN_UAM_maxBolus_breakfast : profile.EN_UAM_maxBolus);
-                        ENMaxSMB = (sens_predType == "UAM+" ? profile.EN_UAMbgBoost_maxBolus : ENMaxSMB)
+                        ENMaxSMB = (sens_predType == "UAM+" ? profile.EN_UAMbgBoost_maxBolus : ENMaxSMB);
                     }
                 }
 
