334ef2ccc992534be2e64b9f012de3a4bb3c212d
DICKO <mark@servecold.net>
2022-10-03 08:02:13 +1100

    UAM+ settings

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/EN/DetermineBasalAdapterENJS.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/EN/DetermineBasalAdapterENJS.kt
@@ -267,6 +267,7 @@ class DetermineBasalAdapterENJS internal constructor(private val scriptReader: S
         this.profile.put("EN_UAM_maxBolus", sp.getDouble(R.string.key_eatingnow_uamboost_maxbolus, 0.0))
         this.profile.put("UAMbgBoost", Profile.toMgdl(sp.getDouble(R.string.key_eatingnow_uambgboost, 0.0),profileFunction.getUnits()))
         this.profile.put("EN_UAMbgBoost_maxBolus", sp.getDouble(R.string.key_eatingnow_uambgboost_maxbolus, 0.0))
+        this.profile.put("EN_UAMPlus_NoENW", sp.getBoolean(R.string.key_use_uamplus_noenw, false))
 
         // Outside of the EN Window ********************************************************************************
         // this.profile.put("COB_maxBolus", sp.getDouble(R.string.key_eatingnow_isfboost_maxbolus, 0.0))
