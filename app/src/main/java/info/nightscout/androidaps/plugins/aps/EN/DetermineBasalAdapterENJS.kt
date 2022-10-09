f3eb9a07fca059ca13aee1f6588a80618da4ffe7
DICKO <mark@servecold.net>
2022-10-10 06:57:54 +1100

    stable determine-basal.js

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/EN/DetermineBasalAdapterENJS.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/EN/DetermineBasalAdapterENJS.kt
@@ -104,9 +104,9 @@ class DetermineBasalAdapterENJS internal constructor(private val scriptReader: S
             //generate functions "determine_basal" and "setTempBasal"
             val enVariant = sp.getString(R.string.key_en_variant, ENDefaults.variant)
             this.profile.put("variant", enVariant);
-            // if (enVariant == ENDefaults.variant)
-            //     rhino.evaluateString(scope, readFile("EN/determine-basal.js"), "JavaScript", 0, null)
-            // else
+            if (enVariant == ENDefaults.variant)
+                rhino.evaluateString(scope, readFile("EN/determine-basal.js"), "JavaScript", 0, null)
+            else
                 rhino.evaluateString(scope, readFile("EN/$enVariant/determine-basal.js"), "JavaScript", 0, null)
 
             rhino.evaluateString(scope, readFile("EN/basal-set-temp.js"), "setTempBasal.js", 0, null)
