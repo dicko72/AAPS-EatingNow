c4ce5dd3342954062269e42812bc330f51e2843e
DICKO <mark@servecold.net>
2022-11-09 17:16:19 +1100

    when no ENW and UAM+ rising fast or bg higher enable ENW

--- app/src/main/assets/EN/latest/determine-basal.js
+++ app/src/main/assets/EN/latest/determine-basal.js
@@ -1232,8 +1232,9 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             // SAFETY: UAM+ fast delta with higher bg lowers eBGw
             eBGweight = (bg > ISFbgMax && delta >= 15 ? 0.30 : eBGweight);
             AllowZT = false; // disable ZT for UAM+
-            // when no ENW and UAM+ at normalTarget + 1mmol enable ENW
-            if (!ENWindowOK && ENactive && bg > normalTarget + 18) ENWindowOK = true;
+
+            // when no ENW and UAM+ enable ENW when bg is higher or bg is rising fast
+            if (!ENWindowOK && ENactive) ENWindowOK = (bg > normalTarget + 18 || bg < ISFbgMax && delta >=15);
         }
 
         // UAM predictions, no COB or GhostCOB
