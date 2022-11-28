ab20617394fb5fc8af4612ebe2bb57847b89ab38
DICKO <mark@servecold.net>
2022-11-29 08:37:09 +1100

    latest dev merge

--- core/interfaces/src/main/java/info/nightscout/interfaces/profile/DefaultValueHelper.kt
+++ core/interfaces/src/main/java/info/nightscout/interfaces/profile/DefaultValueHelper.kt
@@ -10,6 +10,12 @@ interface DefaultValueHelper {
     fun determineEatingSoonTT(): Double
     fun determineEatingSoonTTDuration(): Int
 
+    // Eating Now
+    fun determineEatingNowTTDuration(): Int {
+        val value = sp.getInt(R.string.key_eatingnow_duration, Constants.defaultEatingNowTTDuration)
+        return if (value > 0) value else Constants.defaultEatingNowTTDuration
+    }
+
     /**
      * returns the configured Activity TempTarget, if this is set to 0, the Default-Value is returned.
      *
