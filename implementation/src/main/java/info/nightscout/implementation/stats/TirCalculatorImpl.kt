7a4ff8fb73cde21f7ec9237fd71ab6328246a617
DICKO <mark@servecold.net>
2022-11-30 07:36:43 +1100

    latest dev merge

--- implementation/src/main/java/info/nightscout/implementation/stats/TirCalculatorImpl.kt
+++ implementation/src/main/java/info/nightscout/implementation/stats/TirCalculatorImpl.kt
@@ -30,7 +30,7 @@ class TirCalculatorImpl @Inject constructor(
     private val repository: AppRepository
 ) : TirCalculator {
 
-    override fun calculate(days: Long, lowMgdl: Double, highMgdl: Double): LongSparseArray<TIR> {
+    fun calculate(days: Long, lowMgdl: Double, highMgdl: Double): LongSparseArray<TIR> {
         if (lowMgdl < 39) throw RuntimeException("Low below 39")
         if (lowMgdl > highMgdl) throw RuntimeException("Low > High")
         val startTime = MidnightTime.calc(dateUtil.now() - T.days(days).msecs())
@@ -53,7 +53,7 @@ class TirCalculatorImpl @Inject constructor(
         return result
     }
 
-    private fun averageTIR(tirs: LongSparseArray<TIR>): TIR {
+    fun averageTIR(tirs: LongSparseArray<TIR>): TIR {
         val totalTir = if (tirs.size() > 0) {
             TirImpl(tirs.valueAt(0).date, tirs.valueAt(0).lowThreshold, tirs.valueAt(0).highThreshold)
         } else {
@@ -68,6 +68,29 @@ class TirCalculatorImpl @Inject constructor(
             totalTir.count += tir.count
         }
         return totalTir
+    }
+
+    fun calculateHoursPrior(hrsPriorStart: Long, hrsPriorEnd: Long, lowMgdl: Double, highMgdl: Double): LongSparseArray<TIR> {
+        if (lowMgdl < 39) throw RuntimeException("Low below 39")
+        if (lowMgdl > highMgdl) throw RuntimeException("Low > High")
+        val startTime = dateUtil.now() - T.hours(hour = hrsPriorStart).msecs()
+        val endTime = dateUtil.now() - T.hours(hour = hrsPriorEnd).msecs()
+        val bgReadings = repository.compatGetBgReadingsDataFromTime(startTime, endTime, true).blockingGet()
+
+        val result = LongSparseArray<TIR>()
+        for (bg in bgReadings) {
+            //val midnight = MidnightTime.calc(bg.date)
+            var tir = result[startTime]
+            if (tir == null) {
+                tir = TIR(startTime, lowMgdl, highMgdl)
+                result.append(startTime, tir)
+            }
+            if (bg.value < 39) tir.error()
+            if (bg.value >= 39 && bg.value < lowMgdl) tir.below()
+            if (bg.value in lowMgdl..highMgdl) tir.inRange()
+            if (bg.value > highMgdl) tir.above()
+        }
+        return result
     }
 
     @SuppressLint("SetTextI18n")
