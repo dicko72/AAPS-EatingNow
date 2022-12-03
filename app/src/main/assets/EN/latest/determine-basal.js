f313d66c6f4d1236af1c9afd9cd4fdc6a78aca26
DICKO <mark@servecold.net>
2022-12-04 09:17:06 +1100

    SR_TDD uses TIR_sens overnight
    SR_TDD uses TIR_sens overnight

--- app/src/main/assets/EN/latest/determine-basal.js
+++ app/src/main/assets/EN/latest/determine-basal.js
@@ -351,6 +351,11 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     enlog += "ENactive: " + ENactive + ", ENtimeOK: " + ENtimeOK + "\n";
     enlog += "ENmaxIOBOK: " + ENmaxIOBOK + ", max_iob: " + max_iob + "\n";
 
+    // Threshold for SMB at night
+    var SMBbgOffset = (profile.SMBbgOffset > 0 ? target_bg + profile.SMBbgOffset : target_bg);
+    var ENSleepMode = !ENactive && !ENtimeOK && bg < SMBbgOffset;
+    enlog += "SMBbgOffset:" + SMBbgOffset + "\n";
+
     // patches ===== END
 
     var tick;
@@ -474,30 +479,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     ins_val = (ins_peak < 60 ? (ins_val - ins_peak) + 30 : (ins_val - ins_peak) + 40);
     enlog += "insulinType is " + insulinType + ", ins_val is " + ins_val + ", ins_peak is " + ins_peak + "\n";
 
-    // TDD ********************************
-    // define default vars
-    var SR_TDD = 1, sens_TDD = sens;
-    var TDD = meal_data.TDD;
-
-    // SR_TDD ********************************
-    //var SR_TDD = TDD / meal_data.TDDLastCannula;
-    var SR_TDD = meal_data.TDDLastCannula / meal_data.TDDAvg7d;
-    var sens_LCTDD = 1800 / (meal_data.TDDLastCannula * (Math.log((normalTarget / ins_val) + 1)));
-    sens_LCTDD = sens_LCTDD / (profile.sens_TDD_scale / 100);
-
-    //var SR_TDD = meal_data.TDDLastCannula / meal_data.TDDAvgtoCannula;
-    //var endebug = "AtoC=" + round(meal_data.TDDAvgtoCannula,2) + " LC=" + round(meal_data.TDDLastCannula,2);
-
-    // ISF based on TDD
-    var sens_TDD = 1800 / (TDD * (Math.log((normalTarget / ins_val) + 1)));
-    enlog += "sens_TDD:" + convert_bg(sens_TDD, profile) + "\n";
-    sens_TDD = sens_TDD / (profile.sens_TDD_scale / 100);
-    sens_TDD = (sens_TDD > sens * 3 ? sens : sens_TDD); // fresh install of v3
-    enlog += "sens_TDD scaled by " + profile.sens_TDD_scale + "%:" + convert_bg(sens_TDD, profile) + "\n";
-
-    enlog += "* advanced ISF:\n";
-
-    // TIR_sens - a very simple implementation of autoISF configurable % per hour
+    // ******  TIR_sens - a very simple implementation of autoISF configurable % per hour
     var TIRB0 = 0, TIRB1 = 0, TIRB2 = 0, TIRH_percent = profile.resistancePerHr / 100, TIR_sens = 0, TIR_sens_limited = 0;
 
     // TIRB2 - The TIR for the higher band above 150/8.3
@@ -545,6 +527,31 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     // apply autosens limit to TIR_sens_limited
     TIR_sens_limited = Math.min(TIR_sens, profile.autosens_max);
     TIR_sens_limited = Math.max(TIR_sens_limited, profile.autosens_min);
+    // ******  END TIR_sens - a very simple implementation of autoISF configurable % per hour
+
+    // TDD ********************************
+    // define default vars
+    var SR_TDD = 1, sens_TDD = sens;
+    var TDD = meal_data.TDD;
+
+    // SR_TDD ********************************
+    //var SR_TDD = TDD / meal_data.TDDLastCannula;
+    var SR_TDD = meal_data.TDDLastCannula / meal_data.TDDAvg7d;
+
+    var sens_LCTDD = 1800 / (meal_data.TDDLastCannula * (Math.log((normalTarget / ins_val) + 1)));
+    sens_LCTDD = sens_LCTDD / (profile.sens_TDD_scale / 100);
+
+    //var SR_TDD = meal_data.TDDLastCannula / meal_data.TDDAvgtoCannula;
+    //var endebug = "AtoC=" + round(meal_data.TDDAvgtoCannula,2) + " LC=" + round(meal_data.TDDLastCannula,2);
+
+    // ISF based on TDD
+    var sens_TDD = 1800 / (TDD * (Math.log((normalTarget / ins_val) + 1)));
+    enlog += "sens_TDD:" + convert_bg(sens_TDD, profile) + "\n";
+    sens_TDD = sens_TDD / (profile.sens_TDD_scale / 100);
+    sens_TDD = (sens_TDD > sens * 3 ? sens : sens_TDD); // fresh install of v3
+    enlog += "sens_TDD scaled by " + profile.sens_TDD_scale + "%:" + convert_bg(sens_TDD, profile) + "\n";
+
+    enlog += "* advanced ISF:\n";
 
     // ISF at normal target
     var sens_normalTarget = sens, sens_profile = sens; // use profile sens and keep profile sens with any SR
@@ -582,7 +589,9 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     if (profile.use_sens_TDD || profile.use_sens_LCTDD ) {
         // dont adjust sens_normalTarget
         sensitivityRatio = 1;
-    } else if (profile.enableSRTDD && SR_TDD !=1) {
+    } else if (profile.enableSRTDD) {
+        // SR_TDD overnight uses TIR
+        SR_TDD = (ENSleepMode && TIR_sens_limited !=1 ? TIR_sens_limited : SR_TDD);
         // Use SR_TDD when no TT, profile switch
         sensitivityRatio = (profile.temptargetSet && !ENTTActive || profile.percent != 100 ?  1 : SR_TDD);
         // when SR_TDD shows sensitivity but TIR is resistant reset sensitivityRatio to 100%
@@ -673,11 +682,6 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     // Apply circadian variance to ISF
     sens_normalTarget *= sens_circadian_now;
     enlog += "sens_normalTarget with circadian variance:" + convert_bg(sens_normalTarget, profile) + "\n";
-
-    // Threshold for SMB at night
-    var SMBbgOffset = (profile.SMBbgOffset > 0 ? target_bg + profile.SMBbgOffset : target_bg);
-    var ENSleepMode = !ENactive && !ENtimeOK && bg < SMBbgOffset;
-    enlog += "SMBbgOffset:" + SMBbgOffset + "\n";
 
     // Allow user preferences to adjust the scaling of ISF as BG increases
     // Scaling is converted to a percentage, 0 is normal scaling (1), 5 is 5% stronger (0.95) and -5 is 5% weaker (1.05)
