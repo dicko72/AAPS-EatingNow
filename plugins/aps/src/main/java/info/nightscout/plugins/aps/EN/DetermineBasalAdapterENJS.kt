495f35d4ae3f17dfc0a20e5383994ea3b5325d4d
DICKO <mark@servecold.net>
2023-05-18 18:14:20 +1000

    troubleshoot

--- plugins/aps/src/main/java/info/nightscout/plugins/aps/EN/DetermineBasalAdapterENJS.kt
+++ plugins/aps/src/main/java/info/nightscout/plugins/aps/EN/DetermineBasalAdapterENJS.kt
@@ -346,24 +346,22 @@ class DetermineBasalAdapterENJS internal constructor(private val scriptReader: S
 
         // set the EN start time based on prefs
         val ENStartTime = 3600000 * sp.getInt(R.string.key_eatingnow_timestart, 9) + MidnightTime.calc(now)
-        this.mealData.put("ENStartTime",ENStartTime)
+        // this.mealData.put("ENStartTime",ENStartTime)
 
         // Create array to contain treatment times for ENWStartTime for today
         var ENWStartTimeArray: Array<Long> = arrayOf() // Create array to contain last treatment times for ENW for today
         var ENStartedArray: Array<Long> = arrayOf() // Create array to contain first treatment times for ENStartTime for today
 
-        // // get the FIRST and LAST carb time since EN activation NEW
-        // repository.getCarbsDataFromTimeToTime(ENStartTime,now,false).blockingGet().let { ENCarbs->
-        // // repository.getCarbsDataFromTime(ENStartTime,false).blockingGet().let { ENCarbs->
-        //
-        //     val firstENCarbTime = with(ENCarbs.firstOrNull()?.timestamp) { this ?: 0 }
-        //     this.mealData.put("firstENCarbTime",firstENCarbTime)
-        //     if (firstENCarbTime >0) ENStartedArray += firstENCarbTime
-        //
-        //     val lastENCarbTime = with(ENCarbs.lastOrNull()?.timestamp) { this ?: 0 }
-        //     this.mealData.put("lastENCarbTime",lastENCarbTime)
-        //     ENWStartTimeArray += lastENCarbTime
-        // }
+        // get the FIRST and LAST carb time since EN activation NEW
+        repository.getCarbsDataFromTimeToTime(ENStartTime,now,false).blockingGet().let { ENCarbs->
+            val firstENCarbTime = with(ENCarbs.firstOrNull()?.timestamp) { this ?: 0 }
+            this.mealData.put("firstENCarbTime",firstENCarbTime)
+            if (firstENCarbTime >0) ENStartedArray += firstENCarbTime
+            //
+            // val lastENCarbTime = with(ENCarbs.lastOrNull()?.timestamp) { this ?: 0 }
+            // this.mealData.put("lastENCarbTime",lastENCarbTime)
+            // ENWStartTimeArray += lastENCarbTime
+        }
 
         // // get the FIRST and LAST bolus time since EN activation NEW
         // repository.getENBolusFromTimeOfType(ENStartTime,true, Bolus.Type.NORMAL, enwMinBolus ).blockingGet().let { ENBolus->
