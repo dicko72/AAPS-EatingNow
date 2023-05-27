85e47d9272b90a73623aa62ef16b802193020e08
DICKO <mark@servecold.net>
2023-05-27 14:58:15 +1000

    restrict maxBolus when ENWBolusIOB will be exceeded by SMB fix

--- plugins/aps/src/main/assets/EN/latest/determine-basal.js
+++ plugins/aps/src/main/assets/EN/latest/determine-basal.js
@@ -1395,7 +1395,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     rT.reason += (!ENWindowOK && !ENWTriggerOK && ENtimeOK ? " IOB&lt;" + round(ENWIOBThreshU, 2) : "");
     rT.reason += (ENWindowOK && ENWTriggerOK ? " IOB&gt;" + round(ENWIOBThreshU, 2) : "");
     if (meal_data.ENWBolusIOB) rT.reason += ", ENWBolus:" + round(meal_data.ENWBolusIOB,2) + (ENWBolusIOBMax > 0 ? "/" + ENWBolusIOBMax : "");
-    var endebug = "basaliob:" + iob_data.basaliob;
+//    var endebug = "basaliob:" + iob_data.basaliob;
 
     // other EN stuff
     rT.reason += ", eBGw: " + (sens_predType != "NA" ? sens_predType + " " : "") + convert_bg(insulinReq_bg, profile) + " " + round(eBGweight * 100) + "%";
@@ -1763,7 +1763,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
             // restrict maxBolus when ENWBolusIOB will be exceeded by SMB
             if (ENWBolusIOBMax > 0 && meal_data.ENWBolusIOB + insulinReq > ENWBolusIOBMax) ENMaxSMB = Math.min(ENWBolusIOBMax - meal_data.ENWBolusIOB, ENMaxSMB);
-            if (ENWBolusIOBMax > 0 && meal_data.ENWBolusIOB >= ENWBolusIOBMax) ENMaxSMB = profile.EN_NoENW_maxBolus;
+            if (ENWBolusIOBMax > 0 && meal_data.ENWBolusIOB >= ENWBolusIOBMax) ENMaxSMB = Math.min(ENMaxSMB,profile.EN_NoENW_maxBolus);
 
 
             // ============== TIME BASED RESTRICTIONS ==============
