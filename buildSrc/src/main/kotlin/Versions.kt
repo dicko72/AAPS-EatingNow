298c3273be353b9ebf12a42d70f3ed5491927aa0
DICKO <mark@servecold.net>
2023-12-20 17:00:18 +1100

    3.2.0.3

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,7 +3,7 @@ import org.gradle.api.JavaVersion
 
 object Versions {
 
-    const val appVersion = "3.2.0.2"+"-EatingNow"
+    const val appVersion = "3.2.0.3"+"-EatingNow"
     const val versionCode = 1500
 
     const val ndkVersion = "21.1.6352462"
