fc16c7ae91bd65fbb52eda1184fbeed7eceef647
4RK4N <4rk4nus@googlemail.com>
2023-12-21 22:40:44 +0100

    bump versionCode

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.gradle.api.JavaVersion
 object Versions {
 
     const val appVersion = "3.2.0.3"+"-EatingNow"
-    const val versionCode = 1500
+    const val versionCode = 1803
 
     const val ndkVersion = "21.1.6352462"
 
