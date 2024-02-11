da2e808dbe286cce089ad291b66d53b83a329871
DICKO <mark@servecold.net>
2024-02-11 17:40:02 +1100

    adjust min, max, and target BG for sensitivity with TIR

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -588,8 +588,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         sens_normalTarget = sens_normalTarget / TIR_sens_limited;
     }
 
-    // round SR
-    sensitivityRatio = round(sensitivityRatio, 2);
+    if (sensitivityRatio == 1 && TIRH_percent > 0) sensitivityRatio = TIR_sens_limited; // SR is TIRS if enabled
+    sensitivityRatio = round(sensitivityRatio, 2);     // round SR
 
     basal = round_basal(basal, profile);
     if (basal !== profile_current_basal) {
@@ -603,7 +603,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     if (profile.temptargetSet) {
         //console.log("Temp Target set, not adjusting with autosens; ");
     } else {
-        if (profile.sensitivity_raises_target && (sensitivityRatio < 1 || TIR_sens_limited < 1) || profile.resistance_lowers_target && (sensitivityRatio > 1 || TIR_sens_limited > 1) ) {
+        if (profile.sensitivity_raises_target && sensitivityRatio < 1 || profile.resistance_lowers_target && sensitivityRatio > 1) {
             // with a target of 100, default 0.7-1.2 autosens min/max range would allow a 93-117 target range
             min_bg = round((min_bg - 60) / sensitivityRatio) + 60;
             max_bg = round((max_bg - 60) / sensitivityRatio) + 60;
