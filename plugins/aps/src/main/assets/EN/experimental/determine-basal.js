5ca261e67c9ee2a37a04f8e28c8c8c59d4dbda5c
DICKO <me@myplace.com>
2024-11-06 07:09:44 +1100

    SAFETY: UAMDeltaX delta > 3

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -1291,7 +1291,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             var UAMDeltaX = 0;
             var eBGmax = 230; // safety max of 230mgdl/12.7mmol
             // for lower eventualBg predictions increase eventualBG with UAMDeltaX using current bg as the basis when early on in ENW or less than 80 of ENWBolusIOBMax has been given
-            if (ENWindowOK && ENWStartedAgo < ENWindowDuration) {
+            if (ENWindowOK && ENWStartedAgo < ENWindowDuration && delta > 3) {
                 // Define the range for UAMDeltaX
                 var UAMDeltaXforecast = Math.min(ENWindowDuration,120);
                 var UAMDeltaXboost = (ENPBActive ? 1 : 1);
