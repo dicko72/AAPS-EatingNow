c698f3a8f4335f1f27117efa1b70d49df63e9ee7
DICKO <me@myplace.com>
2025-03-12 11:05:52 +1100

    when resistant allow basal and prevent zero temp

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -1890,8 +1890,9 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             durationReq = round(60 * worstCaseInsulinReq / profile.current_basal);
 
 
-            // when insulinReq is greater than 2 x maxBolus for UAM+ or PB disable ZT
-            //if (ENactive && insulinReq > microBolus *2 && sens_predType == "UAM+" || UAMBGPreBolusUnitsLeft > 0) AllowZT = false;
+            // when resistant allow basal and prevent zero temp
+            if (ENactive && TIR_sens_limited > 1 && insulinReqOrig > 0) AllowZT = false;
+
             // if EN_SMB_percent has reduced the insulinReqPct (insulinReqPctChanged)
             if (insulinReqPctChanged && insulinReq > 0) AllowZT = false;
 
