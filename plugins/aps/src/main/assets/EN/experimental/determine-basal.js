d2b7721d28bf7864be5d2bdf0c440b8cfa84d466
DICKO <me@myplace.com>
2025-03-16 19:52:21 +1100

    allow ENMaxSMB to go up to remaining ENWBolusIOBMax with UAM+ only with ENTT

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -1788,7 +1788,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                 if (sens_predType == "UAM+") ENMaxSMB = Math.max(profile.ENW_maxBolus_UAM_plus, UAMBGPreBolusUnitsLeft);
 
                 // allow ENMaxSMB to go up to remaining ENWBolusIOBMax with UAM+
-                if (ENWBolusIOBRemaining > 0 && sens_predType == "UAM+") ENMaxSMB = Math.max(ENWBolusIOBRemaining,ENMaxSMB);
+                if (ENWBolusIOBRemaining > 0 && sens_predType == "UAM+" && ENTTActive) ENMaxSMB = Math.max(ENWBolusIOBRemaining,ENMaxSMB);
 
             } else {
                 ENMaxSMB = EN_NoENW_maxBolus; // start with the default maxBolus
