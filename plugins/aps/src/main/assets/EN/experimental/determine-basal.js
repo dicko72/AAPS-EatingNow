8344a42fb2c063b46caffa701753de9ed5dc80e6
DICKO <me@myplace.com>
2025-03-17 19:22:11 +1100

    Removed mealscaling criteria from TIRs

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -612,8 +612,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 //        if (profile.percent > 100 && meal_data.TIR0_H_pct == 0) sens_normalTarget *= profile.percent/100; // cancel adjustment if not resistant when switch > 100%
 //    }
 
-    // apply TIRS to ISF only when delta is slight or bg higher when not ISF meal scaling
-    if (TIR_sens_limited !=1 && TIR_sens !=1 && MealScaler !=100) {
+    // apply TIRS to ISF only when delta is slight or bg higher
+    if (TIR_sens_limited !=1 && TIR_sens !=1) {
         sens_normalTarget = sens_normalTarget / TIR_sens_limited;
     }
 
