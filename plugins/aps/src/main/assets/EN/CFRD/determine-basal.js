8f0e5c478cc30a9b67bc0e0b5b267943724bc178
DICKO <me@myplace.com>
2025-04-10 16:37:02 +1000

    CFRD uses total IOB not ENW IOB

--- plugins/aps/src/main/assets/EN/CFRD/determine-basal.js
+++ plugins/aps/src/main/assets/EN/CFRD/determine-basal.js
@@ -415,7 +415,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     //var ENWBolusIOBMax = (firstMealWindow ? profile.ENW_breakfast_max_tdd : profile.ENW_max_tdd); // when EN started + breakfast window time is greater than the latest ENWstartTime there has been no other ENW so still firstmeal only
     var ENWBolusIOBMax = round(profile.ENW_maxIOB,2);
-    var ENWBolusIOB = meal_data.ENWBolusIOB;
+    var ENWBolusIOB = iob_data.iob; // CFRD uses total IOB not ENW IOB
     ENWBolusIOBMax = (ENWindowOK && ENWStartedAgo <= ENWindowDuration ? ENWBolusIOBMax : 0); // reset to 0 if not within ENW
     var ENWBolusIOBRemaining = (ENWBolusIOBMax > 0 && ENWBolusIOB >=0 ? ENWBolusIOBMax - ENWBolusIOB : 0);
     ENWBolusIOBRemaining = Math.max(ENWBolusIOBRemaining, 0); // dont allow negative
