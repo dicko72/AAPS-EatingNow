59af26c8e019f9dd6eaaf2dc1be796293b8ae5e1
DICKO <me@myplace.com>
2025-05-03 02:11:35 +1000

    BG+ revert to only when predicted low

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -1238,7 +1238,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     // Process BG+ first for slight delta when resistant, lower range when asleep
 //    if (TIR_sens_limited > 1 && !ENWindowOK && profile.EN_Use_BGPlus && (insulinReq_bg >= -1.5 * bg && insulinReq_bg <= threshold || minGuardBG >= -1.5 * bg && minGuardBG <= threshold) && delta > -4 && delta <= 6 && glucose_status.long_avgdelta > -2) {
-    if (TIR_sens_limited > 1 && !ENWindowOK && profile.EN_Use_BGPlus && delta > -4 && delta <= 6 && glucose_status.long_avgdelta > -2 && insulinReq_bg <= bg) {
+    if (TIR_sens_limited > 1 && !ENWindowOK && profile.EN_Use_BGPlus && (insulinReq_bg >= -1 * bg && insulinReq_bg <= threshold || minGuardBG >= -1 * bg && minGuardBG <= threshold) && delta > -4 && delta <= 6 && glucose_status.long_avgdelta > -2) {
         // if (TIR_H_safety > 1 || (TIR_M_safety > 1 && (!ENtimeOK || meal_data.TIR_M_pct == 100))) sens_predType = "BG+";
         // if (TIR_H_safety > 1 || TIR_M_safety > 1 && meal_data.TIR_M_pct == 100 && !ENtimeOK) sens_predType = "BG+"; // commenting out as safer BG+ with basal
         if (TIR_H_safety > 1 || TIR_M_safety > 1) sens_predType = "BG+"; // trying as BG+ now uses basal at TIRS%
