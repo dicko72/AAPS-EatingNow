99c1f8c4c81e7cc049357bca046890095d685bd8
DICKO <me@myplace.com>
2025-05-02 19:44:33 +1000

    ENWEndTime

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -405,15 +405,18 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     // set the ENW time since started and ended
     var ENWStartedAgo = (nowUTC - (firstMealWindow ? meal_data.ENStartedTime : meal_data.ENWStartTime)) / 60000;
-    var ENWEndedAgo = ENWStartedAgo-ENWindowDuration;
+    var ENWEndedAgo = (nowUTC - meal_data.ENWEndTime) / 60000;
+//    var ENWEndedAgo = ENWStartedAgo-ENWindowDuration;
 
     // ENWindowOK is when there is a recent COB entry or manual bolus
-    var ENWindowOK = ENactive && ENWStartedAgo < ENWindowDuration;
+    var ENWindowOK = ENactive && ENWEndedAgo < 0; // negative number is how long until it ends
+//    var ENWindowOK = ENactive && ENWStartedAgo < ENWindowDuration;
 
     //var ENWBolusIOBMax = (firstMealWindow ? profile.ENW_breakfast_max_tdd : profile.ENW_max_tdd); // when EN started + breakfast window time is greater than the latest ENWstartTime there has been no other ENW so still firstmeal only
     var ENWBolusIOBMax = round(profile.ENW_maxIOB,2);
     var ENWBolusIOB = Math.max(meal_data.ENWBolusIOB,0);
-    ENWBolusIOBMax = (ENWindowOK && ENWStartedAgo <= ENWindowDuration ? ENWBolusIOBMax : 0); // reset to 0 if not within ENW
+    ENWBolusIOBMax = (ENWindowOK ? ENWBolusIOBMax : 0); // reset to 0 if not within ENW
+//    ENWBolusIOBMax = (ENWindowOK && ENWStartedAgo <= ENWindowDuration ? ENWBolusIOBMax : 0); // reset to 0 if not within ENW
     //var ENWBolusIOBRemaining = (ENWBolusIOBMax > 0 && ENWBolusIOB >=0 ? ENWBolusIOBMax - ENWBolusIOB : 0);
     var ENWBolusIOBRemaining = (ENWBolusIOBMax > 0 ? ENWBolusIOBMax - ENWBolusIOB : 0);
     ENWBolusIOBRemaining = Math.max(ENWBolusIOBRemaining, 0); // dont allow negative
@@ -1297,7 +1300,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             var UAMDeltaX = 0;
             var eBGmax = (ENWBolusIOBRemaining > 0 ? 320 : 230); // safety max of 17.7mmol or 12.7mmol when ENW IOB reached
             // for lower eventualBg predictions increase eventualBG with UAMDeltaX using current bg as the basis when early on in ENW or less than 80 of ENWBolusIOBMax has been given
-            if (ENWindowOK && ENWStartedAgo < ENWindowDuration && (delta > 3 || delta > 0 && ENWBolusIOBRemaining > 0)) {
+            if (ENWindowOK && (delta > 3 || delta > 0 && ENWBolusIOBRemaining > 0)) {
+//            if (ENWindowOK && ENWStartedAgo < ENWindowDuration && (delta > 3 || delta > 0 && ENWBolusIOBRemaining > 0)) {
                 // Define the range for UAMDeltaX
                 var UAMDeltaXforecast = Math.min(ENWindowDuration,120);
                 var UAMDeltaXboost = (ENPBActive ? 1 : 1);
@@ -1435,7 +1439,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     rT.reason += (ENWindowOK ? "On" : "Off");
     rT.reason += (firstMealWindow ? " Bkfst" : "");
     rT.reason += (MealScaler != 100 ? " " + round(MealScaler) + "%" : "");
-    rT.reason += (ENWindowOK && ENWStartedAgo <= ENWindowDuration ? " " + round(ENWStartedAgo) + "/" + ENWindowDuration + "m" : "");
+//    rT.reason += (ENWindowOK && ENWStartedAgo <= ENWindowDuration ? " " + round(ENWStartedAgo) + "/" + ENWindowDuration + "m" : "");
+    rT.reason += (ENWindowOK ? " " + round(ENWStartedAgo) + "/" + ENWindowDuration + "m" : "");
     rT.reason += (PPWindowOK ? " " + round(ENWindowDuration) + "m, " + round(ENWEndedAgo) + "m ago" : "");
     if (meal_data.ENWBolusIOB || ENWindowOK) rT.reason += ", ENW-IOB:" + round(ENWBolusIOB,2) + "/" + round(profile.ENW_maxIOB,2);
 
@@ -1770,7 +1775,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
             var ENinsulinReqPct = 0.75; // EN insulinReqPct is 75%
             var EN_SMB_percent = (ENWindowOK ? profile.ENW_SMB_percent : profile.EN_SMB_percent) /100; // use EN_SMB_percent to indicate EN vs ENW SMB%
             var insulinReqPctChanged = false;
-            var ENWinsulinReqPct = (ENWStartedAgo <= ENWindowDuration ? 1 : ENinsulinReqPct); // ENW insulinReqPct is 100% for the first 30 mins then 85%
+            var ENWinsulinReqPct = (ENWindowOK ? 1 : ENinsulinReqPct); // ENW insulinReqPct is 100% for the first 30 mins then 85%
 
 
             // ============== INSULINREQPCT CHANGES ==============
