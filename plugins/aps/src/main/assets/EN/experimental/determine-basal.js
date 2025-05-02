974e2ce146b9dea810b732a5a8c2f822b3a5d161
DICKO <me@myplace.com>
2025-05-02 20:08:08 +1000

    BG+ if the original TBR is more allow it otherwise give insulinReq

--- plugins/aps/src/main/assets/EN/experimental/determine-basal.js
+++ plugins/aps/src/main/assets/EN/experimental/determine-basal.js
@@ -1817,8 +1817,9 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
                 if (sens_predType == "BG+") {
                     //ENMaxSMB = (profile.current_basal * TIR_sens_limited) / 12;
                     ENMaxSMB = -1;
-                    // if the original TBR is more allow it because resistant
-                    rate = Math.max(rate_orig,profile_current_basal);
+                    // if the original TBR is more allow it otherwise give insulinReq
+                    rate = Math.max(rate_orig,insulinReq * insulinReqPct * 12);
+
                     // when not in the higher band TIRH just use basal rate
                     if (TIR_H_safety == 1) rate = profile_current_basal;
                     AllowZT = false;
