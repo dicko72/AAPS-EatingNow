236e60b083a6f0f6140c6b93a8f2ccd5cab3dce8
DICKO <me@myplace.com>
2026-03-20 19:28:24 +1100

    EN v3.4.1 fix for correct units in reason

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/EN/DetermineBasalEN.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/EN/DetermineBasalEN.kt
@@ -60,11 +60,11 @@ class DetermineBasalEN @Inject constructor(
         return /* expectedDelta */ round(bgi + (targetDelta / fiveMinBlocks), 1)
     }
 
-    fun convert_bg(value: Double): String =
-        profileUtil.stringInCurrentUnitsDetect(value).replace("-0.0", "0.0")
-    //DecimalFormat("0.#").format(profileUtil.fromMgdlToUnits(value))
-    //if (profile.out_units === "mmol/L") round(value / 18, 1).toFixed(1);
-    //else Math.round(value);
+    fun convert_bg(value: Double): String {
+        // Explicitly convert from mg/dL to user units, no guessing!
+        val convertedValue = profileUtil.fromMgdlToUnits(value)
+        return DecimalFormat("0.0#").format(convertedValue).replace("-0.0", "0.0")
+    }
 
     fun enable_smb(profile: OapsProfile, microBolusAllowed: Boolean, meal_data: MealData, target_bg: Double, enConfig: ENConfig, bg: Double): Boolean {
 
@@ -689,17 +689,17 @@ class DetermineBasalEN @Inject constructor(
                 future_sens = (1800 / (ln((((fSensBG * 0.5) + (bg * 0.5)) / profile.insulinDivisor) + 1) * profile.TDD))
                 future_sens = round(future_sens, 1)
                 consoleLog.add("Future state sensitivity is $future_sens based on eventual and current bg due to flat glucose level above target")
-                rT.reason.append("Dosing sensitivity: $future_sens using eventual BG;")
+                rT.reason.append("Dosing sensitivity: " + convert_bg(future_sens) + " using eventual BG;")
             } else if (glucose_status.delta > 0 && eventualBG > target_bg || eventualBG > bg) {
                 future_sens = (1800 / (ln((bg / profile.insulinDivisor) + 1) * profile.TDD))
                 future_sens = round(future_sens, 1)
                 consoleLog.add("Future state sensitivity is $future_sens using current bg due to small delta or variation")
-                rT.reason.append("Dosing sensitivity: $future_sens using current BG;")
+                rT.reason.append("Dosing sensitivity: " + convert_bg(future_sens) + " using current BG;")
             } else {
                 future_sens = (1800 / (ln((fSensBG / profile.insulinDivisor) + 1) * profile.TDD))
                 future_sens = round(future_sens, 1)
                 consoleLog.add("Future state sensitivity is $future_sens based on eventual bg due to -ve delta")
-                rT.reason.append("Dosing sensitivity: $future_sens using eventual BG;")
+                rT.reason.append("Dosing sensitivity: " + convert_bg(future_sens) + " using eventual BG;")
             }
         }
 
@@ -901,7 +901,7 @@ class DetermineBasalEN @Inject constructor(
             rT.reason.append(" and minDelta ${convert_bg(minDelta)} > expectedDelta ${convert_bg(expectedDelta)}; ")
             // predictive low glucose suspend mode: BG is / is projected to be < threshold
         } else if ((bg < threshold || minGuardBG < threshold) && !isPrebolusing) {
-            rT.reason.append("minGuardBG " + convert_bg(minGuardBG) + "<" + convert_bg(threshold))
+            rT.reason.append("minGuardBG " + convert_bg(minGuardBG) + " < " + convert_bg(threshold))
             bgUndershoot = target_bg - minGuardBG
             val worstCaseInsulinReq = bgUndershoot / sens
             var durationReq = round(60 * worstCaseInsulinReq / profile.current_basal)
