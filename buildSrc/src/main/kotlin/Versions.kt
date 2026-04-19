dbe49857596bb19184f2eab22e64cad864de7fcf
DICKO <me@myplace.com>
2026-04-20 07:32:33 +1000

    EN v3.4.2.2 Update to latest branch

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 @Suppress("ConstPropertyName")
 object Versions {
 
-    const val appVersion = "3.4.2.1"+"-EatingNow"
+    const val appVersion = "3.4.2.2"+"-EatingNow"
     const val versionCode = 1500
 
     const val compileSdk = 36
