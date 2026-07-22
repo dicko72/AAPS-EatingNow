edde3b84596f31520dca1c772336da3d65539d35
DICKO <me@myplace.com>
2026-07-22 17:50:43 +1000

    EN v3.4.2.2 isAuthorisedMealRise uses peakIOBmins and relativeActivityPct

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/EN/DetermineBasalEN.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/EN/DetermineBasalEN.kt
@@ -826,8 +826,9 @@ class DetermineBasalEN @Inject constructor(
         val isBasalDeficit = resistanceBudgetLeft > 0.0
         rT.reason.append("* debug: owed ${round(owedSinceHigh, 2)} given ${enConfig.netIOBSinceHigh} left ${round(resistanceBudgetLeft, 2)} cert ${enConfig.minutesHigh}/${certaintyMins}m=${isPersistentRise} *,")
         val isHighTempSet = profile.temptargetSet && target_bg > enConfig.normalTargetBG
-        val isAuthorisedMealRise = (ENWActive || ENWEndedAgoMins in 1 until 60) && !isHighTempSet && (UAMplusConfidence || isPrebolusing || deltaFastUp)
-        // resistance silent until the last window insulin has peaked
+        val isAuthorisedMealRise = (ENWActive || ENWEndedAgoMins in 1 until 60) && !isHighTempSet &&
+            (ENWActive || peakIOBmins == null || peakIOBmins < 0 || relativeActivityPct < 60) &&
+            (UAMplusConfidence || isPrebolusing || deltaFastUp)        // resistance silent until the last window insulin has peaked
         val isAuthorisedResistance = isStuckHigh && isBasalDeficit && !isHighTempSet &&
             !ENWActive && ENWEndedAgoMins >= enConfig.insulinPeakMins
 
